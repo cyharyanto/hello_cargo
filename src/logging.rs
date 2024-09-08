62a5b3662ad94c44f471da895a488d5cbcf2e390
Christoforus Yoga Haryanto <cyharyanto@gmail.com>
2024-09-08 14:02:18 +1000

    Enable HTTP logging middleware.

--- src/logging.rs
+++ src/logging.rs
@@ -1,15 +1,14 @@
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
-use tracing::Level;
 
 pub fn setup_logging(log_level: &str) -> Result<(), Box<dyn std::error::Error>> {
-    log_level.parse::<Level>()?;
-
     let env_filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new(format!("hello_cargo={}", log_level)));
+        .unwrap_or_else(|_| {
+            EnvFilter::new(format!("hello_cargo={},tower_http=debug,axum::rejection=trace", log_level))
+        });
 
     tracing_subscriber::registry()
         .with(env_filter)
-        .with(tracing_subscriber::fmt::layer())
+        .with(tracing_subscriber::fmt::layer().with_target(false))
         .init();
 
     Ok(())
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-mod config;
-mod logging;
-
-use hello_cargo::{app};
+use hello_cargo::app;
 use std::net::SocketAddr;
 use tracing;
+use axum::middleware::map_response;
+use axum::response::Response;
+use tower_http::trace::TraceLayer;
+use tracing::{info, debug};
+
+mod config;
+mod logging;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -11,20 +15,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     logging::setup_logging(&config.log.level)?;
 
-    tracing::info!("Starting application");
-    tracing::debug!("Loaded configuration: {:?}", config);
+    info!("Starting application");
+    debug!("Loaded configuration: {:?}", config);
 
-    let app = app();
+    let app = app().layer(TraceLayer::new_for_http())
+        .layer(map_response(logging_middleware));
 
     let addr = SocketAddr::new(config.server.host, config.server.port);
-    tracing::info!("Listening on {}", addr);
-    tracing::info!("Swagger UI available at http://{}:{}/swagger-ui/", config.server.host, config.server.port);
+    info!("Listening on {}", addr);
+    info!("Swagger UI available at http://{}:{}/swagger-ui/", config.server.host, config.server.port);
 
     axum::serve(tokio::net::TcpListener::bind(addr).await?, app)
         .with_graceful_shutdown(shutdown_signal())
         .await?;
 
-    tracing::info!("Application shutdown complete");
+    info!("Application shutdown complete");
     Ok(())
 }
 
@@ -32,5 +37,10 @@ async fn shutdown_signal() {
     tokio::signal::ctrl_c()
         .await
         .expect("Failed to install CTRL+C signal handler");
-    tracing::info!("Received shutdown signal");
+    info!("Received shutdown signal");
+}
+
+async fn logging_middleware(response: Response) -> Response {
+    debug!("Response status: {}", response.status());
+    response
 }
